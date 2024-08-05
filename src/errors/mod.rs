70dc886fe39f837679d14e09ad1eae1910ec81af
Vincent <vincent.devilers@gmail.com>
2024-08-05 19:54:30 +0200

    delete error

--- src/errors/mod.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error, PartialEq, Clone)]
-pub enum Error {
-    #[error("parsing error : `{0}`")]
-    Parsing(String),
-    #[error("input is empty")]
-    Empty,
-}
