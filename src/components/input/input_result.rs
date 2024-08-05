c101ae73864e3ce63a182e0479f7c8ed80595bf1
Vincent <vincent.devilers@gmail.com>
2024-08-05 20:00:18 +0200

    remove strum because less is more

---
+++ src/components/input/input_result.rs
@@ -0,0 +1,24 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum InputResult<T>
+where
+    T: Display,
+{
+    Result(T),
+    Empty,
+    ParsingError(String),
+}
+
+impl<T> Display for InputResult<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            InputResult::Result(value) => writeln!(f, "{}", value),
+            InputResult::Empty => writeln!(f, ""),
+            InputResult::ParsingError(value) => writeln!(f, "{}", value),
+        }
+    }
+}
